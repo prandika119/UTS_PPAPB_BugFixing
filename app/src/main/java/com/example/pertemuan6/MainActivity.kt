8889018af99a9f1147cabfd2ac721a6439854251
prandika119 <an.prasetya9@gmail.com>
2024-10-09 11:58:33 +0700

    commit

--- app/src/main/java/com/example/pertemuan6/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan6/MainActivity.kt
@@ -3,14 +3,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.DatePicker
 import android.widget.DatePicker.OnDateChangedListener
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.core.view.isInvisible
 import com.example.pertemuan6.databinding.ActivityMainBinding
 import java.util.Calendar
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,6 +30,17 @@ class MainActivity : AppCompatActivity() {
             _tempCalendar.timeInMillis = System.currentTimeMillis()
             var selectedDate = "${_tempCalendar.get(Calendar.DAY_OF_MONTH)} ${monthList[_tempCalendar.get(Calendar.MONTH)]} ${_tempCalendar.get(Calendar.YEAR)}"
 
+            datepicker.init(
+                datepicker.year,
+                datepicker.month,
+                datepicker.dayOfMonth
+            ) { _, year, month, day ->
+                selectedDate = "$day ${monthList[month]} $year"
+            }
+
+            timePicker.setOnTimeChangedListener { view, hour, minute ->
+                selectedTime = String.format("%02d:%02d", hour, minute)
+            }
 
 //            Kehadiran Dropdown=======================================
             val adapterKehadiran = ArrayAdapter<String>(
@@ -42,14 +55,17 @@ class MainActivity : AppCompatActivity() {
                 object : AdapterView.OnItemSelectedListener {
                     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                         var kehadiran = kehadiranList.get(position)
-//                        Toast.makeText(this@MainActivity, "$kehadiran", Toast.LENGTH_SHORT).show()
+
                         if (kehadiran == "Terlambat" || kehadiran =="Izin"){
                             keteranganEdittext.visibility = View.VISIBLE
+                        } else {
+                            keteranganEdittext.visibility = View.INVISIBLE
                         }
                     }
                     override fun onNothingSelected(parent: AdapterView<*>?) {
                     }
                 }
+
 
             submitButton.setOnClickListener {
                 Toast.makeText(this@MainActivity, "Presensi berhasil $selectedDate jam $selectedTime", Toast.LENGTH_SHORT).show()
